6adca945d9781a8374d06c0b356d431f748d5b8d
Gunawan <gunawanramadhan100@yahoo.co.id>
2016-01-27 07:45:56 -0800

    Update login.php

--- login.php
+++ login.php
@@ -13,8 +13,6 @@
 		<form name="login-form" class="login-form" action="config/cek_login.php" method="post">
 			
 		   	<span>Masukan NIP dan Password</span>
-		    </div>
-		    <div class="content">
 			<input type="username" name="nip" class="input username" placeholder="NIP" required="">
 		    <input type="password" name="password" class="input password" placeholder="Password" required="">
 
