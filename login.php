76f6e5a4bf4614b2445eaea6c367611ceb490137
Gunawan <gunawanramadhan100@yahoo.co.id>
2016-01-27 22:13:09 -0800

    login smentara

---
+++ login.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Login form</title>
+<body>
+		<form name="login-form" class="login-form" action="config/cek_login.php" method="post">
+			
+		   	<span>Masukan NIP dan Password</span>
+		    </div>
+		    <div class="content">
+			<input type="username" name="nip" class="input username" placeholder="NIP" required="">
+		    <input type="password" name="password" class="input password" placeholder="Password" required="">
+
+		    <input type="submit" name="submit" value="Login" class="button" />
+		
+		</form>
+			
+</body>
+</html>
+
+	
+<?php
+
+?>
