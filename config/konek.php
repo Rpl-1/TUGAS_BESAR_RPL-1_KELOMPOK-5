ccdd9f5fff2bd259281137459adf90bbe6d02649
Gunawan <gunawanramadhan100@yahoo.co.id>
2016-01-27 21:50:39 -0800

    koneksi ke database

---
+++ config/konek.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(0);
+
+  $server    = "localhost";
+  $username  = "root";
+  $password  = "";
+  $database  = "restoran";
+  
+  mysql_connect($server, $username, $password)
+  		or die ("<h2 align = 'center'><b><font color='red'>Database tidak 	terhubung!!</font></b></h2>");
+		
+		mysql_select_db($database) 
+	     or die ("<h2 align = 'center'><b><font color='red'>Koneksi tidak terbuka!!</font></b></h2>");
+		 
+		 $mysqli = new mysqli($server, $username, $password, $database);  
+?>
